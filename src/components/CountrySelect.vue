58980aff6654cca085c22e541daff8c5cb3fbb56
bobzeee <edegboewoski@gmail.com>
2022-04-25 23:58:22 +0100

    initial commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <select
+    v-model="selected"
+    class="form-select mt-10 block w-full border p-3 rounded"
+    @change="onChange()"
+  >
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :key="country.ID" :value="country.ID">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="grid md:grid-cols-3 gap-4">
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font--bold mb-4">Cases</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ formattedNumbers(stats.NewConfirmed) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ formattedNumbers(stats.TotalConfirmed) }}
+      </div>
+    </div>
+    <!-- Box 2 -->
+    <div class="shadow-md bg-green-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-green-900 font--bold mb-4">Recovered</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ formattedNumbers(stats.NewRecovered) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ formattedNumbers(stats.TotalRecovered) }}
+      </div>
+    </div>
+    <!-- box 3 -->
+    <div class="shadow-md bg-red-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-red-900 font--bold mb-4">Deaths</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ formattedNumbers(stats.NewDeaths) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ formattedNumbers(stats.TotalDeaths) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataBoxes",
+  props: ["stats"],
+  methods: {
+    formattedNumbers(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+  },
+};
+</script>
